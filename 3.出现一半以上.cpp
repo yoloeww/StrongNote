342b578d8b3b8f185042898afbc1ac2178080fef
yolo <77111763+yoloeww@users.noreply.github.com>
2022-11-22 20:30:14 +0800

    Create 3.出现一半以上.cpp

---
+++ 3.出现一半以上.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param numbers int整型vector 
+     * @return int整型
+     */
+    int MoreThanHalfNum_Solution(vector<int>& numbers)
+    {
+      
+        sort(numbers.begin(), numbers.end());
+        return numbers[numbers.size()/2];
+    }
+        // write code here
+};
